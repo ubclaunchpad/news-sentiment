06046a77d16625974cd624ddfa42bd0e0f7152c1
Lucy <hao.lucyy@gmail.com>
2020-10-26 07:50:42 -0600

    added env, go.mod, go.sum, installed mongo drivers, added .gitignore

--- backend/db/db.go
+++ backend/db/db.go
@@ -1,10 +1,27 @@
 package db
 
+import (
+	"log"
+
+	"github.com/joho/godotenv"
+)
+
 // init mongo
 func init() {
+	// load .env file
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file: %s", err.Error())
+	}
+
+	// get credentials
+	// example: mIP := os.Getenv("MONGO_IP")
 	initSession()
 }
 
 // create new ...
+func createNewUser() (string, error) {
+	return "test", nil
+}
 
 // need more specifications on what the user will be doing
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/ubclaunchpad/news-sentiment
+
+go 1.15
+
+require go.mongodb.org/mongo-driver v1.4.2 // indirect
