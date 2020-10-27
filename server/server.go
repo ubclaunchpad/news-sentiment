3a6e5b2e399e121c6312c7a0cf0397fabbbe1ef3
Ahnaf Tazwar Ayub <tazwarahnaf22@gmail.com>
2020-10-27 16:39:43 -0700

    Built simple index;before prose

--- server/server.go
+++ server/server.go
@@ -1,6 +1,5 @@
 package main
 
-// package main
 
 import (
     "fmt"
@@ -8,17 +7,39 @@ import (
     "net/http"
 )
 
+
+func helloHandler(w http.ResponseWriter, r *http.Request){
+
+	if r.URL.Path != "/hello" {
+        http.Error(w, "404 not found.", http.StatusNotFound)
+        return
+    }
+
+    if r.Method != "GET" {
+        http.Error(w, "Method is not supported.", http.StatusNotFound)
+        return
+	}
+	
+
+	fmt.Fprintf(w, "News App")
+
+}
+
+
+
+
 func main() {
 
-	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request){
-		fmt.Fprintf(w, "Hello!")
-	})
+	fileServer := http.FileServer(http.Dir("./static")) // New code
+    http.Handle("/", fileServer) // New code
+	http.HandleFunc("/hello", helloHandler)
 
 
 
 	fmt.Printf("Starting server at port 8080\n")
-
 	if err := http.ListenAndServe(":8080", nil); err != nil {
         log.Fatal(err)
     }
 }
+
+
