54ce5ba583f068af7a851773d3ab60710c1c8f50
Kim DAO <kdaokdao@gmail.com>
2020-10-26 20:06:06 -0700

    installed mongo-driver

--- backend/db/schemas.go
+++ backend/db/schemas.go
@@ -1,5 +1,25 @@
 package db
 
+//import (
+//	"context"
+//	"fmt"
+//	"os"
+//	"time"
+//
+//	"go.mongodb.org/mongo-driver/bson"
+//	"go.mongodb.org/mongo-driver/bson/primitive"
+//	"go.mongodb.org/mongo-driver/mongo"
+//	"go.mongodb.org/mongo-driver/mongo/options"
+//)
+
 // User represents the data that a user would have
 type User struct {
+	ID string
+}
+
+type NewsPiece struct {
+	ID 		string
+	Title 	string
+	Author 	string
+	Source	string
 }
