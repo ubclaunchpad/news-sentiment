f0821513a192c9aecd4fe8f92553c0020c9db392
MayKikuchi <m.may.kikuchi@gmail.com>
2020-10-26 20:35:56 -0700

    initial extension commit

---
+++ frontend/popup/popup.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+    document.querySelector('button').addEventListener('click', onclick, false);
+
+    function onclick() { // currently connected to the button in popup.html with ^ listener
+        chrome.tabs.query({currentWindow: true, active: true},
+            function (tabs) {
+                chrome.tabs.sendMessage(null,'hi');
+            });
+    }
+}, false)
