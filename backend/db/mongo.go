fa28fdac7c09d71077314be9f6d9d0fcb99ff433
Lucy <hao.lucyy@gmail.com>
2020-10-26 21:28:46 -0600

    added insert and fetch method specificaitons

--- backend/db/mongo.go
+++ backend/db/mongo.go
@@ -1,16 +1,28 @@
 package db
 
+// global variable to mongo connection
+
 // make connection with mongo
 func initSession() error {
 	return nil
 }
 
-// insert data into mongo
-func insert() error {
+// insert User into mongo
+func insertUser(new User) error {
 	return nil
 }
 
-// fetch data from mongo
-func fetch() error {
+// insert NewsPiece associated with User into mongo
+func insertNewsPiece(new NewsPiece, associatedUser User) error {
+	return nil
+}
+
+// fetch User into mongo
+func fetchUser(findUser User) (error, User) {
+	return nil
+}
+
+// fetch NewsPiece associated with User from  mongo
+func fetchNewsPiece(findNews NewsPiece, associatedUser User) (error, User, NewsPiece) {
 	return nil
 }
