1abe6ac061c0c8aae7cd801c6aa06b3ee255121f
MayKikuchi <m.may.kikuchi@gmail.com>
2020-10-27 15:40:20 -0700

    remove inner popup.js stuff

--- frontend/popup/popup.js
+++ frontend/popup/popup.js
@@ -1,12 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
 
     document.querySelector('button').addEventListener('click', onclick, false);
-
-    function onclick() { // currently connected to the button in popup.html with ^ listener
-        chrome.tabs.query({currentWindow: true, active: true},
-            function (tabs) {
-                chrome.tabs.sendMessage(null,'hi');
-                alert('hellooooo');
-            });
-    }
+    
 }, false)
