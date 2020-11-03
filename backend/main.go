ca2142a6b3a2fd45196437eefd23cdf3fd2634cd
Ahnaf Tazwar Ayub <tazwarahnaf22@gmail.com>
2020-11-03 09:59:33 -0800

    GET individuals; cleaned useless comments

--- backend/main.go
+++ backend/main.go
@@ -4,9 +4,6 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"io/ioutil"
-	"strings"
-
 	"github.com/joho/godotenv"
 	db "github.com/ubclaunchpad/news-sentiment/db"
 	"github.com/gorilla/mux"
@@ -24,30 +21,22 @@ func homePage(w http.ResponseWriter, r *http.Request){
 
 
 //GET: endpoint for a single user
-//Just print stuff
-// MAKE TYPE SAFE
 func getUser(w http.ResponseWriter, req *http.Request){
-	id := strings.TrimPrefix(req.URL.Path, "/users/")
-	fmt.Fprintf(w, id)
-	
-	user := db.User {
-		ID:    id,
-		Name:  "sdsfsfsd",
-		Email: "email",
-	}
 
-	// db.fetchUser(user)
-	fmt.Fprintf(w, user.Name)
+	vars := mux.Vars(req)
+	id := vars["id"]
+	fmt.Fprintf(w, "Requesting details for user: " + id)
+
 }
 
 
 //GET: endpoint for a single article
 func getArticle(w http.ResponseWriter, req *http.Request){
-	reqBody, err := ioutil.ReadAll(req.Body)
-    if err != nil {
-        log.Fatal(err)
-    }
-    fmt.Printf("%s", reqBody)
+
+	vars := mux.Vars(req)
+	id := vars["id"]
+	fmt.Fprintf(w, "Requesting details for article: " + id)
+
 }
 
 
@@ -58,15 +47,10 @@ func handleRequests(){
 	// creates a new instance of a mux router
 	router := mux.NewRouter().StrictSlash(true)
 	
-    // replace http.HandleFunc with router.HandleFunc
-    router.HandleFunc("/", homePage)
-	// router.HandleFunc("/all", returnAllArticles)
-	
+	router.HandleFunc("/", homePage)
+	router.HandleFunc("/users/{id}", getUser)
+	router.HandleFunc("/articles/{id}", getArticle)
 
-
-    // finally, instead of passing in nil, we want
-    // to pass in our newly created router as the second
-    // argument
     log.Fatal(http.ListenAndServe(":8090", router))
 }
 
@@ -79,20 +63,7 @@ func main() {
 	}
 	db.Init()
 
+	fmt.Println("Starting Server...")
 	//Controls all endpoints
 	handleRequests()
-	// // 
-	// http.HandleFunc("/hello", hello)
-	// http.HandleFunc("/headers", headers)
-
-	// //API Route stubs--set em up...
-	// http.HandleFunc("/users/:id", getUser)
-	// http.HandleFunc("/articles/:id", getArticle)
-	//PUT POST GET DEL... (User Article)
-
-
-
-
-	// fmt.Println("Running server on port 8090")
-	// http.ListenAndServe(":8090", nil)
 }
