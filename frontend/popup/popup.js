d1b2da85569f4f308b7768aab9786f5329165a25
michelleteh <nmteh17@students.cs.ubc.ca>
2020-11-16 13:39:14 +0700

    trial for slider

--- frontend/popup/popup.js
+++ frontend/popup/popup.js
@@ -66,4 +66,13 @@ document.addEventListener('DOMContentLoaded', function() {
         chrome.tabs.create({ url: url });
     }
 
+    var slider = document.getElementById("myRange");
+    var output = document.getElementById("demo");
+    output.innerHTML = slider.value; // Display the default slider value
+
+    // Update the current slider value (each time you drag the slider handle)
+    slider.oninput = function() {
+    output.innerHTML = this.value;
+}
+
 }, false);
